985809e2120a3b3407b7578decd74ac6722714a2
brucetoo <brucetoo14@gmail.com>
2017-10-17 22:38:25 +0800

    1.Use ClipDrawable to show progress 2.Morph animated bg by GradientDrawable

--- app/src/main/java/com/brucetoo/listvideoplay/MainActivity.java
+++ app/src/main/java/com/brucetoo/listvideoplay/MainActivity.java
@@ -1,12 +1,17 @@
 package com.brucetoo.listvideoplay;
 
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.brucetoo.listvideoplay.demo.DetailFragment;
 import com.brucetoo.listvideoplay.demo.ListViewFragment;
@@ -18,66 +23,114 @@ import com.brucetoo.listvideoplay.demo.RecyclerViewSmallScreenFragment;
 import com.brucetoo.listvideoplay.demo.SpringAnimationFragment;
 import com.brucetoo.videoplayer.tracker.Tracker;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        final ClipDrawable drawable = (ClipDrawable) (findViewById(R.id.clip_image)).getBackground();
+//        drawable.setLevel(0);
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                drawable.setLevel(drawable.getLevel() + 50);
+//                handler.postDelayed(this, 500);
+//            }
+//        }, 500);
 
+        final GradientDrawable gd = new GradientDrawable();
+        gd.setColor(getResources().getColor(R.color.colorAccent));
+        final float max = getResources().getDimension(R.dimen.dimen_max);
+        float min = getResources().getDimension(R.dimen.dimen_min);
+        float dp1 = getResources().getDimension(R.dimen.dimen_1);
+        gd.setShape(GradientDrawable.RECTANGLE);
+        gd.setCornerRadius(min);
+        final ImageView view = (ImageView) findViewById(R.id.clip_image);
+        view.setImageDrawable(gd);
+
+        ValueAnimator radius = ValueAnimator.ofFloat(min, max);
+        radius.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (float) animation.getAnimatedValue();
+                gd.setCornerRadius(value);
+            }
+        });
+        radius.setRepeatMode(ValueAnimator.REVERSE);
+        radius.setRepeatCount(ValueAnimator.INFINITE);
+
+        ValueAnimator width = ValueAnimator.ofFloat(100 * dp1, 60 * dp1);
+        width.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float value = (float) animation.getAnimatedValue();
+                view.getLayoutParams().width = (int) value;
+                view.requestLayout();
+            }
+        });
+        width.setRepeatMode(ValueAnimator.REVERSE);
+        width.setRepeatCount(ValueAnimator.INFINITE);
+
+        AnimatorSet set = new AnimatorSet();
+        set.setDuration(2000);
+        set.playTogether(width, radius);
+        set.start();
     }
 
 
-  public void onListViewClick(View view) {
+    public void onListViewClick(View view) {
 
         getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.layout_container, new ListViewFragment(), "ListViewFragment")
-                .addToBackStack(null)
-                .commit();
+            .beginTransaction()
+            .replace(R.id.layout_container, new ListViewFragment(), "ListViewFragment")
+            .addToBackStack(null)
+            .commit();
     }
 
     public void onRecyclerViewClick(View view) {
         getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.layout_container, new RecyclerViewFragment(), "RecyclerViewFragment")
-                .addToBackStack(null)
-                .commit();
+            .beginTransaction()
+            .replace(R.id.layout_container, new RecyclerViewFragment(), "RecyclerViewFragment")
+            .addToBackStack(null)
+            .commit();
     }
 
     public void onSmallScreenClick(View view) {
         getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.layout_container, new RecyclerViewSmallScreenFragment(), "RecyclerViewSmallScreenFragment")
-                .addToBackStack(null)
-                .commit();
+            .beginTransaction()
+            .replace(R.id.layout_container, new RecyclerViewSmallScreenFragment(), "RecyclerViewSmallScreenFragment")
+            .addToBackStack(null)
+            .commit();
     }
 
     public void onSmallScreenClick1(View view) {
         getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.layout_container, new ListViewSmallScreenFragment(), "ListViewSmallScreenFragment")
-                .addToBackStack(null)
-                .commit();
+            .beginTransaction()
+            .replace(R.id.layout_container, new ListViewSmallScreenFragment(), "ListViewSmallScreenFragment")
+            .addToBackStack(null)
+            .commit();
     }
 
     public void onHighLightClick(View view) {
         getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.layout_container, new ListViewMaskFragment(), "ListViewMaskFragment")
-                .addToBackStack(null)
-                .commit();
+            .beginTransaction()
+            .replace(R.id.layout_container, new ListViewMaskFragment(), "ListViewMaskFragment")
+            .addToBackStack(null)
+            .commit();
     }
 
     public void onSpringAnimation(View view) {
         getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.layout_container, new SpringAnimationFragment(), "SpringAnimationFragment")
-                .addToBackStack(null)
-                .commit();
+            .beginTransaction()
+            .replace(R.id.layout_container, new SpringAnimationFragment(), "SpringAnimationFragment")
+            .addToBackStack(null)
+            .commit();
     }
 
-    public void onVideoSupport(View view){
+    public void onVideoSupport(View view) {
         getSupportFragmentManager()
             .beginTransaction()
             .replace(R.id.layout_container, new PagerSupportFragment(), "PagerSupportFragment")
@@ -85,10 +138,10 @@ public class MainActivity extends AppCompatActivity{
             .commit();
     }
 
-    public void addDetailFragment(){
+    public void addDetailFragment() {
         getSupportFragmentManager()
             .beginTransaction()
-            .add(R.id.layout_container,new DetailFragment(),"DetailFragment")
+            .add(R.id.layout_container, new DetailFragment(), "DetailFragment")
             .addToBackStack("DetailFragment")
             .commitAllowingStateLoss();
     }
@@ -96,13 +149,13 @@ public class MainActivity extends AppCompatActivity{
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        Tracker.onConfigurationChanged(this,newConfig);
+        Tracker.onConfigurationChanged(this, newConfig);
     }
 
     @Override
     public void onBackPressed() {
-        if(getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-                && getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE) {
+        if (getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            && getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE) {
             // Get the current fragment using the method from the second step above...
             Fragment currentFragment = getCurrentFragment();
 
@@ -123,7 +176,7 @@ public class MainActivity extends AppCompatActivity{
             // Any other logic needed...
             // call super method to be sure the back button does its thing...
             super.onBackPressed();
-        }else {
+        } else {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         }
     }

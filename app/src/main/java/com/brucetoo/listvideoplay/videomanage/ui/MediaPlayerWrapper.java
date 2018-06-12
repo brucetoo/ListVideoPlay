f726a4e9089246a074ea79b59b694a2af714e3d3
brucetoo <brucetoo14@gmail.com>
2018-06-12 15:08:56 +0800

    fix not in ui thread..

--- app/src/main/java/com/brucetoo/listvideoplay/videomanage/ui/MediaPlayerWrapper.java
+++ app/src/main/java/com/brucetoo/listvideoplay/videomanage/ui/MediaPlayerWrapper.java
@@ -147,9 +147,6 @@ public abstract class MediaPlayerWrapper
     @Override
     public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
         if (SHOW_LOGS) Logger.v(TAG, "onVideoSizeChanged, width " + width + ", height " + height);
-        if (!inUiThread()) {
-            throw new RuntimeException("this should be called in Main Thread");
-        }
         if (mListener != null) {
             mListener.onVideoSizeChangedMainThread(width, height);
         }
